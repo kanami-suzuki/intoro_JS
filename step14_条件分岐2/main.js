a336d342b7ef2855e1455a00a2f6ac63f387c1bd
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-06-02 21:00:17 +0900

    else if

--- step14_条件分岐2/main.js
+++ step14_条件分岐2/main.js
@@ -1,2 +1,23 @@
 'use strict';
 //else if
+
+//例題
+//テストの点数Xによって、
+//通知表の成績をコンソールに
+//出力するプログラムを書く
+
+// 90点以上 A
+// 80点以上 B
+// 60点以上 C
+// 60点未満 D
+
+let X = 60;
+if (X >= 90) {
+  console.log('A');
+} else if (X >= 80) {
+  console.log('B');
+} else if (X >= 60) {
+  console.log('C');
+} else {
+  console.log('D');
+}
