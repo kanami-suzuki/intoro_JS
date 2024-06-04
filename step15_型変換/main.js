78dab2979a9e994a16dafdc6e829cb1d668ba3eb
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-06-04 21:15:27 +0900

    boolean型

--- step15_型変換/main.js
+++ step15_型変換/main.js
@@ -1,25 +1,10 @@
 'use strict';
-let birthYear = '2000';
-let age = 18;
+let b = true;
+if (b) {
+  console.log('はい');
+} else {
+  console.log('いいえ');
+}
 
-console.log(birthYear + age);
-console.log(age + birthYear);
-//文字列の結合になる
-
-//'文字列' + 数値 = '文字列'
-//数値 + '文字列' = '文字列'
-//数値 + 数値 = 数値
-//'文字列' + '文字列' = '文字列'
-
-//文字列型の値を強制的に数値にする
-//Number()数値型以外のデータ型を数値型に変換することができる
-//値が数値に変換できない場合はNaNが帰ってくる
-//NaNは「Not a Number」の略
-console.log(Number(birthYear) + age);
-console.log(Number('Nakamura'));
-
-console.log(age + '歳');
-
-//String()
-//文字列型に変換する
-console.log(String(age) + '歳');
+//論理判定の前にエクスクラメーション(!)をつけると逆の値が出力される
+console.log(!b);
