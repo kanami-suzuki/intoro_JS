ecf673f24c3f1542acbf785cc29975dc41a535e1
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-06-23 20:56:57 +0900

    while文、do while文

---
+++ step38_do while/main.js
@@ -0,0 +1,14 @@
+'use strict';
+//例題１：do while文を使用してコンソールに1〜3の数字を表示する
+let i = 1;
+do {
+  console.log(i);
+  i++
+} while (i <= 3)
+
+let j = 4;
+do {
+  console.log(i);
+  i++
+} while (j <= 3)
+//式が成り立たないので一回で終わり
