dbaf27748928dcb974dd2a558dbb4eb1a6059331
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-06-28 22:07:10 +0900

    テキストの置換

--- section2/step42_要素Nodeを書き換える/main.js
+++ section2/step42_要素Nodeを書き換える/main.js
@@ -1,15 +1,10 @@
 'use strict';
-//ボタンをクリックしたら処理を実行する
+//ボタンをクリックしたらテキストを変更する
 let btn = document.getElementById('triggerButton');
-btn.addEventListener('click', function (e) { //イベント処理
-  // idによるノードの取得
+btn.addEventListener('click', function (e) {
   let headerTitle = document.getElementById('headerTitle');
-  console.log(headerTitle);
-
-  //コンテンツ(テキスト)を取得
-  console.log(headerTitle.textContent);
+  //テキストの置換
+  headerTitle.textContent = 'タイトルが変更されました';
 }, false)
-//getElementById()メソッド(関数)
-//引数に指定した文字列がHTML上にidとして一致した場合に要素を取得できる
-//Elementは日本語で要素。直訳すると「idの要素を取得する」のような感じになる
-//一致する要素がないときはnullが返ってくる
+
+//textContentプロパティでブラウザ上のテキストを変更することができる
