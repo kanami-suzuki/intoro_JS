e3b48c73a0414f648fe73cf26b5fd08e26fb7466
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-06-29 12:44:53 +0900

    getAttribute()メソッド

--- section2/step42_要素Nodeを書き換える/main.js
+++ section2/step42_要素Nodeを書き換える/main.js
@@ -2,9 +2,18 @@
 //ボタンをクリックしたらテキストを変更する
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function (e) {
-  let headerTitle = document.getElementById('headerTitle');
-  //テキストの置換
-  headerTitle.textContent = 'タイトルが変更されました';
-}, false)
+  //idによるノードの取得
+  let inputColorName = document.getElementById('inputColorName');
 
-//textContentプロパティでブラウザ上のテキストを変更することができる
+  //取得したinput要素の入力値を取得
+  let colorNameText = inputColorName.value;
+
+  //取得したinput要素のname属性値を取得
+  //getAttribute()メソッドを使用し、取得したい属性値を引数に入力する
+  let inputName = inputColorName.getAttribute('name');
+
+  //showText(p要素)のidを取得
+  let showText = document.getElementById('showText');
+
+  showText.textContent = `name属性: ${inputName}, 入力されたテキスト： ${colorNameText}`;
+}, false)
